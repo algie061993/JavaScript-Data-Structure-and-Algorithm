8c35b4857b116b91c3c94c240739e5afbcd55929
algie061993 <algie061993@gmail.com>
2025-09-28 23:19:57 +0800

    Grading program (unfinish)

--- Exercises 2/JavaScript/script.js
+++ Exercises 2/JavaScript/script.js
@@ -1,15 +1,18 @@
+// exercise 2  building a gradebook app
+
 function getAverage(scores) {
+  // if the scores array is empty
   if (scores.length === 0) {
     return 0;
   }
 
-  let sum = 0;
-
+  let sum = 0; // variable to store the sum
+  // loop through the scores array
   for (let i = 0; i < scores.length; i++) {
-    sum += scores[i];
+    sum += scores[i]; // add the current score to the sum
   }
 
-  return sum / scores.length;
+  return sum / scores.length; // return the average
 }
 
 // shorter way to find the average
@@ -27,6 +30,7 @@ function getAverage(scores) {
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 
+// function to get the grade
 function getGrade(score) {
   if (score === 100) {
     return "A++";
@@ -48,9 +52,11 @@ console.log("82" + getGrade(82));
 console.log("56" + getGrade(56));
 console.log("78" + getGrade(78));
 
+// function to check if the student has a passing grade
 function hasPassingGrade(score) {
+  // if the student has a failing grade
   if (getGrade(score) === "F") {
-    return false;
+    return false; // return false
   }
   return true;
 }
@@ -63,9 +69,10 @@ console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
 
+// function to get the student message if the student has a passing grade or failed
 function studentMsg(totalScores, studentScore) {
-  const average = getAverage(totalScores);
-  const grade = getGrade(studentScore);
+  const average = getAverage(totalScores); // variable to store the average of the total scores array invoked the getAverage function
+  const grade = getGrade(studentScore); // variable to store the grade of the studentScore invoked the getGrade function
   return `Class average: ${average}. Your grade: ${grade}. ${
     grade === "F" ? "You failed the course." : "You passed the course."
   }`;
