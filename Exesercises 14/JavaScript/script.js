f47263ed2328e19e520e02a01c5faab2f62626ea
algie061993 <algie061993@gmail.com>
2026-01-28 16:07:11 +0800

    unfinish

---
+++ Exesercises 14/JavaScript/script.js
@@ -0,0 +1,17 @@
+/**
+ * Statistics is a way of using math to make sense of data.
+ * It helps us understand patterns and trends in information,
+ * so we can make predictions and decisions based on that information.
+ *
+ * In this challenge, you will build a statistics calculator that takes
+ * a set of numbers and returns the mean, median, mode, standard deviation, and variance.
+ */
+
+const getMean = (array) => {
+  const sum = array.reduce();
+};
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+};
